3712e83d2ab968efca9672c6484276d50c7d207e
Laurent <l0lo86@free.fr>
2021-04-06 16:58:55 +0200

    Test script to check asset bundle workflow

---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LoadAssetBundle : MonoBehaviour
+{
+    AssetBundle assetBundle;
+
+    public void LoadAssetBundleAtPath(string path)
+    {
+        assetBundle = AssetBundle.LoadFromFile(path);
+        Debug.Log(assetBundle == null ? "Failed to load Asset Bundle" : "Asset bundle loaded succesfully");
+
+        assetBundle.LoadAllAssets();
+    }
+}
